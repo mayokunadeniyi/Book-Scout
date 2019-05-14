02af35a0f113380346321040026770f8b726a5a1
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-05-14 11:35:49 +0100

    Add: Picasso Library

---
+++ app/src/main/java/Adapter/BookRecyclerViewAdapter.java
@@ -0,0 +1,64 @@
+package Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mayokun.bookscout.R;
+
+import java.util.List;
+
+import Model.Book;
+
+public class BookRecyclerViewAdapter extends RecyclerView.Adapter<BookRecyclerViewAdapter.ViewHolder> {
+    private List<Book> bookList;
+    private Context context;
+
+    public BookRecyclerViewAdapter(Context context, List<Book> list){
+        this.bookList = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public BookRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item,viewGroup,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookRecyclerViewAdapter.ViewHolder viewHolder, int i) {
+        Book book = bookList.get(i);
+        String posterLink = book.getPoster();
+
+        viewHolder.bookName.setText(book.getBookTitle());
+        viewHolder.bookAuthor.setText(book.getBookAuthor());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return bookList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView bookCover;
+        public TextView bookName;
+        public TextView bookAuthor;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            bookCover = (ImageView) itemView.findViewById(R.id.bookImageID);
+            bookAuthor = (TextView) itemView.findViewById(R.id.bookAuthor);
+            bookName = (TextView) itemView.findViewById(R.id.bookTitleID);
+        }
+    }
+}
