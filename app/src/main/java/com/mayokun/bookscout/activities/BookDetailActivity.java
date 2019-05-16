da58d507aa2e143d00a98c31be6448d8345a760e
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-05-16 09:43:58 +0100

    Added Share Intent to BookDetailActivity

--- app/src/main/java/com/mayokun/bookscout/activities/BookDetailActivity.java
+++ app/src/main/java/com/mayokun/bookscout/activities/BookDetailActivity.java
@@ -1,6 +1,12 @@
 package com.mayokun.bookscout.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -23,6 +29,11 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import model.Book;
 import utils.Constants;
@@ -76,19 +87,19 @@ public class BookDetailActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                shareIntent();
             }
         });
     }
 
-    public void getBookDetails(String id){
+    public void getBookDetails(String id) {
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
                 Constants.BASE_LEFT_URL + id + Constants.BASE_RIGHT_URL, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
 
-                        if (response.has("publishers")){
+                        if (response.has("publishers")) {
                             try {
                                 JSONArray publishersArray = response.getJSONArray("publishers");
                                 int arrayLength = publishersArray.length();
@@ -96,22 +107,22 @@ public class BookDetailActivity extends AppCompatActivity {
                                 for (int i = 0; i < arrayLength; i++) {
                                     publishers[i] = publishersArray.getString(i);
                                 }
-                                bookPublisher.setText(TextUtils.join(",",publishers));
+                                bookPublisher.setText(TextUtils.join(",", publishers));
 
-                            }catch (JSONException e){
+                            } catch (JSONException e) {
                                 e.printStackTrace();
                             }
-                        }else {
+                        } else {
                             bookPublisher.setText(getString(R.string.publisher_na));
                         }
 
-                        if (response.has("number_of_pages")){
+                        if (response.has("number_of_pages")) {
                             try {
                                 bookPages.setText(String.format("%s Pages", Integer.toString(response.getInt("number_of_pages"))));
-                            }catch (JSONException e){
+                            } catch (JSONException e) {
                                 e.printStackTrace();
                             }
-                        }else {
+                        } else {
                             bookPages.setText(getString(R.string.pages_na));
                         }
 
@@ -120,11 +131,52 @@ public class BookDetailActivity extends AppCompatActivity {
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                VolleyLog.d("Error",error.getMessage());
+                VolleyLog.d("Error", error.getMessage());
             }
         });
         queue.add(jsonObjectRequest);
 
+    }
+
+    private void shareIntent() {
+        ImageView shareImage = (ImageView) findViewById(R.id.bookImg);
+        TextView shareTitle = (TextView) findViewById(R.id.titleID);
+
+        Uri bmpUri = getLocalBitmapUri(shareImage);
+        // Construct a ShareIntent with link to image
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("*/*");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, (String) shareTitle.getText());
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        //Launc share menu
+        startActivity(Intent.createChooser(shareIntent, "Share Image"));
+    }
+
+    private Uri getLocalBitmapUri(ImageView shareImage) {
+        //Extract Bitmap from ImageView drawable
+        Drawable drawable = shareImage.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable) {
+            bmp = ((BitmapDrawable) shareImage.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                    "share_image" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
+            outputStream.close();
+            bmpUri = Uri.fromFile(file);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
     }
 
 
