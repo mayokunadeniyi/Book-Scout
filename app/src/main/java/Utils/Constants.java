a0e0e6c1c246063ab00de6f38bbd37ce0ff00b37
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-05-17 19:46:02 +0100

    Changed color primary and color primaryDark

--- app/src/main/java/Utils/Constants.java
+++
@@ -1,6 +0,0 @@
-package Utils;
-
-public class Constants {
-    private static final String LEFT_BASE_URL = "http://openlibrary.org/search.json?q=";
-    private static final String RIGHT_BASE_URL = "&limit=20";
-}
