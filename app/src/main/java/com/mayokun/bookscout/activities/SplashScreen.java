9bee48ca9c581f2be58ea546e6bf1f6a54171900
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-05-18 23:16:56 +0100

    Added Splash Screen

--- app/src/main/java/com/mayokun/bookscout/activities/SplashScreen.java
+++ app/src/main/java/com/mayokun/bookscout/activities/SplashScreen.java
@@ -1,5 +1,7 @@
 package com.mayokun.bookscout.activities;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,13 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },1500);
     }
 }
